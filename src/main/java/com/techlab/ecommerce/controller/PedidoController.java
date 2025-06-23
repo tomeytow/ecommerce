d71a25243905a3ab7e5016504410f2b838d90dd0
Tomas Gomez <gomeztomassalvador@gmail.com>
2025-06-23 12:08:18 -0300

    Update PedidoController.java

--- src/main/java/com/techlab/ecommerce/controller/PedidoController.java
+++ src/main/java/com/techlab/ecommerce/controller/PedidoController.java
@@ -1,9 +1,21 @@
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+
+import com.techlab.ecommerce.service.PedidoService;
+import com.techlab.ecommerce.model.Pedido;
+import com.techlab.ecommerce.model.LineaPedido;
+
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoController {
 
     private final PedidoService servicio;
-
+    
     public PedidoController(PedidoService servicio) {
         this.servicio = servicio;
     }
